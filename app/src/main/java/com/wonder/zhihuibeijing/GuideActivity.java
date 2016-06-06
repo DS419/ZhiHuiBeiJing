3f5c45ceb7979bce7007c35c5e059c71d0c7bd6a
Wonderland_ds <1525863556@qq.com>
2016-06-07 01:25:47 +0800

    添加引导页的功能实现

--- app/src/main/java/com/wonder/zhihuibeijing/GuideActivity.java
+++ app/src/main/java/com/wonder/zhihuibeijing/GuideActivity.java
@@ -2,15 +2,63 @@ package com.wonder.zhihuibeijing;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by 13036 on 2016/6/6.
  */
 public class GuideActivity extends Activity{
 
+    private ViewPager viewPager;
+
+    private List<ImageView> imageViewList = new ArrayList<ImageView>();
+
+    private int[] mImageIds = {R.drawable.guide_1, R.drawable.guide_2, R.drawable.guide_3};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_guide);
+
+        viewPager = (ViewPager) findViewById(R.id.vp_guide);
+
+        for (int i = 0; i < mImageIds.length; i++) {
+            ImageView imageView = new ImageView(this);
+            imageView.setBackgroundResource(mImageIds[i]);
+            imageViewList.add(imageView);
+        }
+
+        PagerAdapter pagerAdapter = new PagerAdapter() {
+            @Override
+            public int getCount() {
+                return imageViewList.size();
+            }
+
+            @Override
+            public boolean isViewFromObject(View view, Object object) {
+                return view == object;
+            }
+
+            @Override
+            public void destroyItem(ViewGroup container, int position, Object object) {
+                container.removeView(imageViewList.get(position));
+            }
+
+            @Override
+            public Object instantiateItem(ViewGroup container, int position) {
+                container.addView(imageViewList.get(position));
+                return imageViewList.get(position);
+            }
+        };
+        viewPager.setAdapter(pagerAdapter);
     }
 }
