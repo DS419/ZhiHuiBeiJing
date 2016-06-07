e13371aa0e61229bf8ee4feffc577a7ddef2df1f
Wonderland_ds <1525863556@qq.com>
2016-06-07 11:24:15 +0800

    添加页面指示器的小圆点功能

--- app/src/main/java/com/wonder/zhihuibeijing/GuideActivity.java
+++ app/src/main/java/com/wonder/zhihuibeijing/GuideActivity.java
@@ -4,10 +4,14 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.view.Window;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,9 +23,15 @@ public class GuideActivity extends Activity{
 
     private ViewPager viewPager;
 
+    private ImageView ivRedPoint;
+
+    private LinearLayout llContainer;
+
     private List<ImageView> imageViewList = new ArrayList<ImageView>();
 
     private int[] mImageIds = {R.drawable.guide_1, R.drawable.guide_2, R.drawable.guide_3};
+
+    private int mPointDis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +40,10 @@ public class GuideActivity extends Activity{
         setContentView(R.layout.activity_guide);
 
         viewPager = (ViewPager) findViewById(R.id.vp_guide);
+        llContainer = (LinearLayout) findViewById(R.id.ll_container);
+        ivRedPoint = (ImageView) findViewById(R.id.iv_red_point);
 
-        for (int i = 0; i < mImageIds.length; i++) {
-            ImageView imageView = new ImageView(this);
-            imageView.setBackgroundResource(mImageIds[i]);
-            imageViewList.add(imageView);
-        }
+        initData();
 
         PagerAdapter pagerAdapter = new PagerAdapter() {
             @Override
@@ -60,5 +68,58 @@ public class GuideActivity extends Activity{
             }
         };
         viewPager.setAdapter(pagerAdapter);
+
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                //当页面正在滑动时被调用
+                Log.d("GuideActivity", "位置：" + String.valueOf(position));
+                Log.d("GuideActivity", "移动百分比：" + String.valueOf(positionOffset));
+                int leftMargin = (int) (mPointDis * positionOffset + position * mPointDis);
+                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams
+                        (RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                params.leftMargin = leftMargin;
+                ivRedPoint.setLayoutParams(params);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                //当页面被选中时被调用
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                //当页面状态改变时被调用
+            }
+        });
+
+        ivRedPoint.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                ivRedPoint.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                mPointDis = llContainer.getChildAt(1).getLeft() - llContainer.getChildAt(0).getLeft();
+                Log.d("GuideActivity", "间距：" + String.valueOf(mPointDis));
+            }
+        });
+
+    }
+
+    private void initData() {
+        for (int i = 0; i < mImageIds.length; i++) {
+            ImageView imageView = new ImageView(this);
+            imageView.setBackgroundResource(mImageIds[i]);
+            imageViewList.add(imageView);
+
+            ImageView point = new ImageView(this);
+            point.setBackgroundResource(R.drawable.shape_point_gray);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams
+                    (LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            if (i > 0) {
+                params.leftMargin = 10;
+            }
+
+            point.setLayoutParams(params);
+            llContainer.addView(point);
+        }
     }
 }
