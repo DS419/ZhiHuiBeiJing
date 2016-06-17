c42d6145ece9eee436b14466ab689ccf0a38f135
Wonderland_ds <1525863556@qq.com>
2016-06-17 15:11:22 +0800

    使用Xutils获取json

--- app/src/main/java/com/wonder/zhihuibeijing/base/impl/NewsCenterPager.java
+++ app/src/main/java/com/wonder/zhihuibeijing/base/impl/NewsCenterPager.java
@@ -6,8 +6,15 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
+import com.lidroid.xutils.http.client.HttpRequest;
 import com.wonder.zhihuibeijing.base.BasePager;
+import com.wonder.zhihuibeijing.global.GlobalConstants;
 
 /**
  * Created by 13036 on 2016/6/10.
@@ -31,5 +38,23 @@ public class NewsCenterPager extends BasePager{
 
         tvTitle.setText("新闻");
         btnMenu.setVisibility(View.VISIBLE);
+
+        getDataFromServer();
+    }
+
+    private void getDataFromServer() {
+        HttpUtils utils = new HttpUtils();
+        utils.send(HttpRequest.HttpMethod.GET, GlobalConstants.CATEGORY_URL, new RequestCallBack<String>() {
+            @Override
+            public void onSuccess(ResponseInfo<String> responseInfo) {
+                Log.d("NewsCenterPager", String.valueOf(responseInfo.result));
+            }
+
+            @Override
+            public void onFailure(HttpException e, String s) {
+                e.printStackTrace();
+                Toast.makeText(mActivity, s, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/wonder/zhihuibeijing/global/GlobalConstants.java
@@ -0,0 +1,11 @@
+package com.wonder.zhihuibeijing.global;
+
+/**
+ * Created by 13036 on 2016/6/12.
+ */
+public class GlobalConstants {
+
+    public static final String SERVER_URL = "http://192.168.196.2:8080/zhbj";
+
+    public static final String CATEGORY_URL = SERVER_URL + "/categories.json";
+}
